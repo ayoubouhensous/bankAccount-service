baf567d2f0b4e367070772c08cdf88057cadf64b
ouhensous <ayoub.ouhen@gmail.com>
2025-10-19 13:44:28 +0100

    expose BankAccount via Spring Data REST and add AccountProjection

---
+++ src/main/java/com/sid/bankAccount_service/entities/AccountProjection.java
@@ -0,0 +1,18 @@
+package com.sid.bankAccount_service.entities;
+
+
+import com.sid.bankAccount_service.enums.AccountType;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(types = BankAccount.class , name = "p1")
+public interface AccountProjection {
+
+    public String getId();
+    public AccountType getType();
+
+    public Double getBalance();
+
+
+
+
+}
--- src/main/java/com/sid/bankAccount_service/repositories/BankAccountRepository.java
+++ src/main/java/com/sid/bankAccount_service/repositories/BankAccountRepository.java
@@ -1,8 +1,18 @@
 package com.sid.bankAccount_service.repositories;
 
 import com.sid.bankAccount_service.entities.BankAccount;
+import com.sid.bankAccount_service.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+
+    @RestResource(path = "/byType")
+    List<BankAccount> findByType(@Param("t") AccountType type);
 
 }
